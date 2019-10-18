405d9d450ade489a6155dc22c632325612a18e10
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2019-10-17 22:13:31 -0700

    Added A* and teleporters

--- mavesolver/MazeSolver.java
+++ mavesolver/MazeSolver.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class MazeSolver {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.println("Enter width:");
         int width = sc.nextInt();
         System.out.println("Enter height:");
@@ -18,6 +18,9 @@ public class MazeSolver {
 
         // Track start and end now so we do not have to loop through whole thing to find them later
         Square start = null, end = null;
+
+        // Store teleporter locations if the maze has them
+        ArrayList<Square> teleporters = new ArrayList<>();
 
         System.out.println("Now enter your maze:");
         int count = 0;
@@ -49,27 +52,36 @@ public class MazeSolver {
                         }
                     }
 
+                    if (tile.isTeleport()) {
+                        teleporters.add(tile);
+                    }
+
                     maze[count][i] = tile;
                 }
                 count++;
             } else {
                 // Don't error if invalid line input given so allows so copypasting file contents which may have had other text or comments
-                System.out.println("Valid mazes can must have " + width + " characters per line and only consist of # (Wall), . (Open), o (Start), * (End)");
+                System.out.println("Valid mazes can must have " + width + " characters per line and only consist of # (Wall), . (Open), o (Start), * (End), @ (Teleporter)");
             }
         }
         
+        System.out.println("You entered the following maze:");
         displayMaze(maze);
-        
+
         // Make sure maze has both a start and end
         if (start == null || end == null) throw new IllegalArgumentException("Valid mazes must have a start and an end!");
-        solveMaze(maze, start, end, width, height);
+        // There can only be 0 or 2 teleporters
+        if (!(teleporters.size() == 0 || teleporters.size() == 2)) throw new IllegalArgumentException("Valid mazes only have 0 or 2 teleporters!");
 
+        solveMaze(maze, start, end, width, height, teleporters);
+
+        // Stop memory leak by closing scanners
         sc.close();
     }
 
     public static boolean validInput (String s, int width) {
-        // Regex is checking if all characters are part of set [#.ox]
-        return s.length() == width && s.matches("^[#.o*]+$");
+        // Regex is checking if all characters are part of set [#.ox@]
+        return s.length() == width && s.matches("^[#.o*@]+$");
     }
 
     public static void displayMaze (Square[][] maze) {
@@ -82,11 +94,32 @@ public class MazeSolver {
         }
     }
 
-    public static void solveMaze (Square[][] maze, Square start, Square end, int width, int height) {
-        // Use a Queue so can do BFS rather than DFS - doing BFS allows for not having to keep track of distances
-        // since every tile is reached in the minimum distance from start
-        Queue<Square> queue = new LinkedList<>();
+    public static int chooseHeuristic(Square next, Square end, char mode) {
+        /*
+        mode = 'e': Euclidean distance from current tile to end
+        mode = 'm': Manhattan distance from current tile to end
+        mode = 'p': Proximity sensor -> Manhattan distance if lower than 8, 8 if higher
+        anything else: 0
+         */
+        int diffX = Math.abs(next.getX() - end.getX());
+        int diffY = Math.abs(next.getY() - end.getY());
+        if (mode == 'e') {
+            return (int) Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
+        } else if (mode == 'm') {
+            return diffX + diffY;
+        } else if (mode == 'p') {
+            return Math.min(diffX + diffY, 8);
+        } else {
+            return 0;
+        }
+    }
+
+    public static void solveMaze (Square[][] maze, Square start, Square end, int width, int height, ArrayList<Square> teleporters) {
+        // PriorityQueue allows use of heuristics to guess which tiles will lead to the end, and therefore should be explored earlier
+
+        PriorityQueue<Square> queue = new PriorityQueue<>();
         queue.add(start);
+        start.setDistance(0);
 
         // HashSet since inserting into it is O(1) rather than ArrayList.contains() being O(n)
         HashSet<Square> seen = new HashSet<>();
@@ -94,18 +127,44 @@ public class MazeSolver {
 
         boolean pathFound = false;
 
+        // Chooses mode that will be used in calls to chooseHeuristic()
+        char hMode = 'e';
+
+        /*
+        If a heuristic mode is selected, this may not try all values leading to potentially not finding the shortest path
+        Example:
+        .............
+        .@.o.....*.@.
+        .............
+        with any heuristic mode selected it will go across rather than using the teleporter
+        */
+
+        if (!teleporters.isEmpty() && new HashSet<>(Arrays.asList('e', 'm', 'p')).contains(hMode)) {
+            System.out.println("Attention! Your maze has teleporters and you have selected a heuristic. Keep in mind this may result in the shortest path not being found");
+        }
+
+        int count = 0;
         // If queue is empty and path is not found, that means there is no possible path to end
+
         while (!queue.isEmpty()) {
             // Get first element of queue and remove it
             Square curr = queue.poll();
             int currX = curr.getX();
             int currY = curr.getY();
 
-            /* Checks to make sure that will not cause ArrayIndexOutOfBounds exception, then get cells on one side of current one
-               Make sure never have seen cell before to stop infinite loops and then add to queue and add to set of seen squares
-               Set each squares adj to be which square led to it
+            // Count has no functional use except to see if the heuristics are working better than having none
+            count++;
+
+            /*
+            Checks to make sure that will not cause ArrayIndexOutOfBounds exception, then get cells on one side of current one
+            Make sure never have seen cell before to stop infinite loops and then add to queue and add to set of seen squares
+            Set each squares adj to be which square led to it
+            A* Algorithm: Set the squares heuristic value to be the distance from start plus the estimated distance to end
             */
 
+            //TODO: Add tie breaker for heuristics
+
+            // LEFT
             if (currX > 0) {
                 Square next = maze[currY][currX-1];
                 if (next.isEnd()) {
@@ -115,10 +174,13 @@ public class MazeSolver {
                 }
                 if (next.isOpen() && seen.add(next)) {
                     next.setAdj(curr);
+                    next.setDistance(curr.getDistance()+1);
+                    next.setHeuristic(chooseHeuristic(next, end, hMode)+next.getDistance());
                     queue.add(next);
                 }
             }
-
+            
+            // UP
             if (currY > 0) {
                 Square next = maze[currY-1][currX];
                 if (next.isEnd()) {
@@ -128,10 +190,13 @@ public class MazeSolver {
                 }
                 if (next.isOpen() && seen.add(next)) {
                     next.setAdj(curr);
+                    next.setDistance(curr.getDistance()+1);
+                    next.setHeuristic(chooseHeuristic(next, end, hMode)+next.getDistance());
                     queue.add(next);
                 }
             }
 
+            // RIGHT
             if (currX < width-1) {
                 Square next = maze[currY][currX+1];
                 if (next.isEnd()) {
@@ -141,10 +206,13 @@ public class MazeSolver {
                 }
                 if (next.isOpen() && seen.add(next)) {
                     next.setAdj(curr);
+                    next.setDistance(curr.getDistance()+1);
+                    next.setHeuristic(chooseHeuristic(next, end, hMode)+next.getDistance());
                     queue.add(next);
                 }
             }
 
+            // DOWN
             if (currY < height-1) {
                 Square next = maze[currY+1][currX];
                 if (next.isEnd()) {
@@ -154,6 +222,22 @@ public class MazeSolver {
                 }
                 if (next.isOpen() && seen.add(next)) {
                     next.setAdj(curr);
+                    next.setDistance(curr.getDistance()+1);
+                    next.setHeuristic(chooseHeuristic(next, end, hMode)+next.getDistance());
+                    queue.add(next);
+                }
+            }
+            
+            // TELEPORTER
+            // Make sure that the teleporter list has a size of 2, meaning no teleporter has been seen
+            if (curr.isTeleport() && teleporters.size() > 1) {
+                // Remove seen teleporter so the tiles cannot keep jumping back to each other and making an infinite loop
+                teleporters.remove(curr);
+                // Teleporter goes directly to other teleporter
+                Square next = teleporters.get(0);
+                if (seen.add(next)) {
+                    next.setAdj(curr);
+                    next.setHeuristic(chooseHeuristic(next, end, hMode));
                     queue.add(next);
                 }
             }
@@ -161,13 +245,17 @@ public class MazeSolver {
 
         if (pathFound) {
             ArrayList<Square> path = findPath(end);
-            // Nice way of printing path out, anything on the path is marked as "p" and anything else that is not the start or end is "_"
+            // path includes the start so subtract 1 to not include that
+            System.out.println("The solution was " + (path.size()-1) + " tiles long! (includes end)");
+            // Nice way of printing path out, anything on the path is marked as "p" and anything else that is not the start, end or teleporter is "_"
             for (Square[] row : maze) {
                 for (Square item : row) {
                     if (item.isStart()) {
                         System.out.print("o");
                     } else if (item.isEnd()) {
                         System.out.print("*");
+                    } else if (item.isTeleport()) {
+                        System.out.print("@");
                     } else if (path.contains(item)) {
                         System.out.print("p");
                     } else {
@@ -179,6 +267,8 @@ public class MazeSolver {
         } else {
             System.out.println("No path exists");
         }
+
+        System.out.println("There were " + count + " tiles searched");
     }
 
     public static ArrayList<Square> findPath(Square end) {
--- mavesolver/Square.java
+++ mavesolver/Square.java
@@ -3,11 +3,11 @@ package mazesolver;
 /**
  Chose to make Square a class rather than an enum to be able to track various important variables
  Being able to track adj is very important for backtracking from end to start to find path
- Also having unique object references is very useful for working in sets and comparison
-**/
+ Distance is important for A* and also unique object references for the HashSet
+ **/
 
-public class Square {
-    private int x, y;
+public class Square implements Comparable<Square> {
+    private int x, y, distance, heuristic;
     private char mark;
     private Square adj;
 
@@ -18,7 +18,8 @@ public class Square {
     }
 
     public boolean isOpen () {
-        return mark=='.';
+        // You do not have to teleport if on a teleporter, you can pass through it
+        return mark=='.' || mark == '@';
     }
 
     public boolean isStart () {
@@ -29,6 +30,10 @@ public class Square {
         return mark=='*';
     }
 
+    public boolean isTeleport () {
+        return mark=='@';
+    }
+
     public int getX() {
         return x;
     }
@@ -37,17 +42,33 @@ public class Square {
         return y;
     }
 
-
-    public String toString() {
-        // Override toString method so it prints the correct character rather than the object reference
-        return Character.toString(mark);
-    }
-
     public Square getAdj() {
         return adj;
     }
 
     public void setAdj(Square adj) {
         this.adj = adj;
+    }
+
+    public void setHeuristic(int heuristic) {
+        this.heuristic = heuristic;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public String toString() {
+        // Override toString method so it prints the correct character rather than the object reference
+        return Character.toString(mark);
+    }
+
+    public int compareTo(Square o) {
+        // used in PriorityQueue to order elements, the lower the heuristic the higher the priority
+        return this.heuristic - o.heuristic;
     }
 }
